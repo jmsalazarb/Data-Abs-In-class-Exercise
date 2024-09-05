90a906468d73df1b359ade63132f364d5da1222c
jmsalazarb <144841696+jmsalazarb@users.noreply.github.com>
2024-09-05 11:01:39 -0500

    Update day2.java

--- day2.java
+++ day2.java
@@ -8,22 +8,26 @@ public class day2 {
         Stack<Character> stack = new Stack<>();
         boolean flag = false;
 
-        for(char c : test4.toCharArray()){
-            if (c == '(' || c == '[' || c == '{'){
+        for (char c : test3.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
                 stack.push(c);
-            } else if (c == ')' || c == ']' || c == '}'){
-                char top = stack.pop();
-                if ((top == '(' && c == ')') || (top == '[' && c == ']') || (top == '{' && c == '}') && stack.isEmpty()){
-                    flag = true;
+            } else if (c == ')' || c == ']' || c == '}') {
+                if (stack.isEmpty()) {
+                    flag = false;  // No opening bracket to match
                     break;
-                }else{
-                    flag = false;
+                }
+                char top = stack.pop();
+                if (!((top == '(' && c == ')') || (top == '[' && c == ']') || (top == '{' && c == '}'))) {
+                    flag = false;  // Mismatch found
                     break;
                 }
             }
         }
 
+        // Check if the stack is empty after processing all characters
+        if (!stack.isEmpty()) {
+            flag = false;
+        }
         System.out.println("Flag: " + flag);
-
     }
 }
