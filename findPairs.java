81a874fcd553bec49a48e44bf097f940aae61290
jmsalazarb <144841696+jmsalazarb@users.noreply.github.com>
2024-10-02 13:33:38 -0500

    Add files via upload

---
+++ findPairs.java
@@ -0,0 +1,19 @@
+public class findPairs {
+    HashMap<Integer, Integer> pairMap = new HashMap<>();
+    for (int[] pair : pairs) {
+        int a = pair[0];
+        int b = pair[1];
+
+    }
+    if (pairMap.containsKey(b) && pairMap.get(b) == a) {
+        System.out.println(a + ", " + b);
+    } else {
+        pairMap.put(a, b);
+    }
+
+    public static void main(String args []){
+        System.out.println(findPairs());
+    }
+}
+
+
