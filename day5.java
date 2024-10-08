d44796df85878e7f084e617ce67def378f13e7b7
jmsalazarb <144841696+jmsalazarb@users.noreply.github.com>
2024-10-08 10:43:33 -0500

    Update day5.java

--- day5.java
+++ day5.java
@@ -18,4 +18,29 @@ public class Node {
         }
         return curr.val;
     }
+    public boolean isBalanced(Node root){
+        int lh;
+
+        int rh;
+
+        if (node == null) {
+            return true;
+        }
+
+        lh = height(node.left);
+        rh = height(node.right);
+
+        if (Math.Abs(lh - rh) <= 1 && isBalanced(node.left)
+            && isBalanced(node.right)) {
+            return true;
+        }
+
+        return false;
+    }
+    public int height(Node nd){
+        if(nd == null){
+            return 0;
+        }
+        return 1 + Math.Max(height(nd.left),height(nd.right));
+    }
 }
